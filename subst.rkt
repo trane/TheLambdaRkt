76db09167c3c691ed7cee548a2af44526f84cd37
Andrew Kuhnhausen <trane@xmission.com>
2012-03-08 19:11:59 -0700

    Add alpha conversion

--- subst.rkt
+++ subst.rkt
@@ -17,7 +17,7 @@
 ;'(λ (y) (y ((λ (z) u))))
 (define (ß-reduce term)
   (match term
-    [`((λ (,v1) ,b1), (and rhs `(λ (,v2) ,b2))) (subst b1 v1 rhs)]))
+    [`((λ (,v1) ,b1) ,(and rhs `(λ (,v2) ,b2))) (subst b1 v1 rhs)]))
 
 ;> (full-ß-reduce `((λ(y) (y a))((λ(x)x)(λ(z)((λ(u)u)z)))))
 ;'((λ (z) ((λ (u) u) z)))
@@ -28,5 +28,32 @@
     [(? symbol?) (error "Cannot reduce value" term )]
     [`(λ (,v) ,v) (term)]
     [`((λ (,v1) ,b1)(λ (,v2) ,b2)) (ß-reduce term)]
-    [`((λ (,v1) ,b1), e) (full-ß-reduce e)]
+    [`((λ (,v1) ,b1), e) (full-ß-reduce e)]))
+
+(define (alpha-conversion term)
+  (match term
+    ; if λx.x -> λy.y
+    [`(λ (,v) ,v) (define v1 (gensym 'v)) `(λ (,v1) ,v1)]
+    ; if λx.λx.x -> λy.λx.x
+    [`(λ (,v1) ,b1) (define v2 (gensym 'v1)) 
+                    (if (eq? (car b1) 'λ)
+                        ; if λx.λx.x -> λx'.λx''.x''
+                        `(λ (,v2) ,(alpha-conversion b1))
+                        ; if λx.xy -> λx'.x'y
+                        `(λ (,v2) ,(subst b1 v1 v2)))]
+    ))
+
+(define (a-conv term)
+  (match term
+    [(? symbol?) term]
+    [`(λ (,v1) ,b) (define v2 (gensym v1)) `(λ (,v2) ,(a-conv (subst b v1 v2)))]
+    [`(,f ,e) `( ,(a-conv f) ,(a-conv e))]))
+
+
+(define (cbv-reduce term)
+  (match term
+    [(? symbol?) (error "Cannot reduce value" term)]
+    [`(λ (,v) ,v) (term)]
+    [`((λ (,v1) ,b1)(λ (,v2) ,b2)) (ß-reduce term)]
+    [`((λ (,v1), b1), e) (cbv-reduce e)]
     [`(,f ,e) (full-ß-reduce f)]))
